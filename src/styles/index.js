168ad7197cd9b27b243cf4c9d5414c269249dee0
Mark John Allen Nizal <154728620+marknizal@users.noreply.github.com>
2025-09-01 09:43:51 +0800

    setup global css & styles

---
+++ src/styles/index.js
@@ -0,0 +1,21 @@
+export const breakpoint = {
+  mobile: "@media only screen and (max-width: 767px)",
+  tablet: "@media only screen and (min-width: 768px) and (max-width: 1279px)",
+  desktop: "@media only screen and (min-width: 1280px)",
+};
+
+export const radius = {
+  sm: "0.5rem",
+  md: "0.75rem",
+  base: "1rem",
+  lg: "2rem",
+  full: "50%",
+};
+
+export const color = {
+  primary: "#",
+  white: "#",
+  black: "#000000",
+  transparent: "rgba(0,0,0,0)",
+  link: "#2A9DF4",
+};
